35986dd22e5943693450ed10d0ae8c8bdc5c6d33
Yakun Wang <yakun_wang@htc.com>
2016-11-15 11:12:57 +0800

    update code generator to use unit64 coding

--- chromaTagGenerator/robustChroma.py
+++ chromaTagGenerator/robustChroma.py
@@ -84,7 +84,6 @@ def image2Matrix(imageMatrix):
         matrix.append(row)
     return matrix
 
-
 '''
 Creates an image from a matrix
 '''
@@ -97,9 +96,8 @@ def generateImage(name, matrix):
         for j in range(len(matrix)):
             for k in range(scale):
                 for t in range(scale):
-                    img.putpixel((i*scale + t, j*scale + k), matrix[i][j])
+                    img.putpixel((j*scale + k, i*scale + t), matrix[i][j])
     img.save(name, "PNG")
-
 
 '''
 Creates a black and white image
@@ -187,88 +185,142 @@ def runTest(name, test):
 #        print '\nOutput != Input!'
 #        print 'Failed Test %s!\n\n' % (name)
 
-
-print '\n\n---- TESTING CODE -----\n\n'
-test = [[1, 0],[0, 1]]
-runTest('#1', test) 
-
-test2 = [[0, 1, 1], [1, 1, 1], [0, 1, 0]]
-runTest('#2', test2)
-
-apriltag36_11_12 = [[0,0,0,0,0,0,0,0,0,0],\
-                    [0,1,1,1,1,1,1,1,1,0],\
-                    [0,1,1,1,1,1,0,1,1,0],\
-                    [0,1,1,0,1,0,0,0,1,0],\
-                    [0,1,0,1,1,0,1,1,1,0],\
-                    [0,1,0,0,1,1,0,1,1,0],\
-                    [0,1,1,0,1,0,1,1,1,0],\
-                    [0,1,1,0,1,0,0,1,1,0],\
-                    [0,1,1,1,1,1,1,1,1,0],\
-                    [0,0,0,0,0,0,0,0,0,0]]
-
-runTest('tag36_11_12', apriltag36_11_12)
-
-apriltag36_11_13 = [[0,0,0,0,0,0,0,0,0,0],\
-                    [0,1,1,1,1,1,1,1,1,0],\
-                    [0,1,0,1,1,1,0,1,1,0],\
-                    [0,1,1,1,0,1,0,0,1,0],\
-                    [0,1,0,1,0,0,1,0,1,0],\
-                    [0,1,0,0,1,1,1,1,1,0],\
-                    [0,1,0,1,1,1,1,0,1,0],\
-                    [0,1,0,0,0,0,0,1,1,0],\
-                    [0,1,1,1,1,1,1,1,1,0],\
-                    [0,0,0,0,0,0,0,0,0,0]]
-
-runTest('tag36_11_13', apriltag36_11_13)
-
-apriltag36_11_14 = [[0,0,0,0,0,0,0,0,0,0],\
-                    [0,1,1,1,1,1,1,1,1,0],\
-                    [0,1,0,0,1,1,0,1,1,0],\
-                    [0,1,0,1,1,0,1,0,1,0],\
-                    [0,1,1,1,0,1,0,0,1,0],\
-                    [0,1,1,1,0,1,1,0,1,0],\
-                    [0,1,0,0,0,0,1,1,1,0],\
-                    [0,1,0,1,1,0,1,0,1,0],\
-                    [0,1,1,1,1,1,1,1,1,0],\
-                    [0,0,0,0,0,0,0,0,0,0]]
-
-runTest('tag36_11_14', apriltag36_11_14)
-
-apriltag36_11_80 = [[0,0,0,0,0,0,0,0,0,0],\
-                    [0,1,1,1,1,1,1,1,1,0],\
-                    [0,1,1,0,0,1,0,1,1,0],\
-                    [0,1,0,0,0,0,1,0,1,0],\
-                    [0,1,1,1,1,1,0,1,1,0],\
-                    [0,1,1,0,0,1,1,1,1,0],\
-                    [0,1,0,1,1,1,1,1,1,0],\
-                    [0,1,1,0,1,0,1,1,1,0],\
-                    [0,1,1,1,1,1,1,1,1,0],\
-                    [0,0,0,0,0,0,0,0,0,0]]
-
-runTest('tag36_11_80', apriltag36_11_80)
-
-
-apriltag36_11_544= [[0,0,0,0,0,0,0,0,0,0],\
-                    [0,1,1,1,1,1,1,1,1,0],\
-                    [0,1,0,0,0,0,1,1,1,0],\
-                    [0,1,1,0,1,1,1,0,1,0],\
-                    [0,1,0,0,1,0,0,1,1,0],\
-                    [0,1,0,1,1,0,0,1,1,0],\
-                    [0,1,1,0,0,0,1,1,1,0],\
-                    [0,1,1,1,0,0,1,1,1,0],\
-                    [0,1,1,1,1,1,1,1,1,0],\
-                    [0,0,0,0,0,0,0,0,0,0]]
-
-runTest('tag36_11_544', apriltag36_11_544)
-
-
-test4 = []
-print 'Enter 6 rows, each row MUST contain 6 entries of 1s and 0s ONLY!'
-print 'EXAMPLE: 1 0 1 0 1 0'
 '''
-for i in range(6):
-    row = input('Row #%s: ' % (i))
-    row = row.split()
-    test4.append(row)
-runTest('#4', test4)
+  Convert to two-dimensional array
 '''
+def toTwoDArray(numin, length):
+    resMatrix = []
+    while numin > 0:
+        num = numin & 0b11111
+        numin = numin >> length
+        line = []
+        for i in range(length):
+            if num & 0b1:
+                line.append(0)
+            else:
+                line.append(1)
+            num = num >> 1
+        resMatrix.append(line[::-1])
+    if(len(resMatrix) < length):
+        resMatrix.extend([[0] * length for row in range(length - len(resMatrix))])
+    return resMatrix[::-1]
+
+'''
+  Add outliers to the two-dimensional array
+'''
+def addOutliers(numin):
+    resMatrix = []
+    length = len(numin)
+    # top
+    resMatrix.append([0] * (length + 4))
+    resMatrix.append([1] * (length + 4))
+    resMatrix[1][0] = 0
+    resMatrix[1][-1] = 0
+    # mid
+    for i in range(length):
+        line = [0, 1]
+        line.extend(numin[i])
+        line.extend([1, 0])
+        resMatrix.append(line)
+    # bottom
+    resMatrix.append([1] * (length + 4))
+    resMatrix[-1][0] = 0
+    resMatrix[-1][-1] = 0        
+    resMatrix.append([0] * (length + 4))
+    return resMatrix
+
+Tag25h9 = [0x25b7abL,  0x1d2c8faL, 0x1e130d4L, 0x703699L,  0x1776a86L, 0xc91d61L,
+           0xb245c4L,  0x16c8317L, 0x1a02a68L, 0xbbbe46L,  0x21d112L,  0x162a467L,
+           0x1748cacL, 0x11fa4bL,  0x1c503b1L, 0x1898f2eL, 0x125c0f7L, 0x59d7c2L,
+           0x127e50dL, 0x9bf985L,  0xd8722cL,  0x13261b3L, 0x1ae71d9L, 0x15dd109L,
+           0x189a1f2L, 0x17b27acL, 0x5edf55L,  0x182b92bL, 0x70ed4eL,  0x11d8ae0L,
+           0x13be6dbL, 0xeeab84L,  0x48b4e8L,  0xd40e82L,  0x93503bL,  0x7d53beL]
+
+print '\n---- TESTING Tag25h9 -----\n\n'
+# print addOutliers(toTwoDArray(Tag25h9[0], 5))
+for i in range(len(Tag25h9)):
+    runTest('Tag25h9-' + str(i), addOutliers(toTwoDArray(Tag25h9[i], 5)))
+
+# test = [[1, 0],[0, 1]]
+# runTest('#1', test) 
+
+# test2 = [[0, 1, 1], [1, 1, 1], [0, 1, 0]]
+# runTest('#2', test2)
+
+# apriltag36_11_12 = [[0,0,0,0,0,0,0,0,0,0],\
+#                     [0,1,1,1,1,1,1,1,1,0],\
+#                     [0,1,1,1,1,1,0,1,1,0],\
+#                     [0,1,1,0,1,0,0,0,1,0],\
+#                     [0,1,0,1,1,0,1,1,1,0],\
+#                     [0,1,0,0,1,1,0,1,1,0],\
+#                     [0,1,1,0,1,0,1,1,1,0],\
+#                     [0,1,1,0,1,0,0,1,1,0],\
+#                     [0,1,1,1,1,1,1,1,1,0],\
+#                     [0,0,0,0,0,0,0,0,0,0]]
+
+# runTest('tag36_11_12', apriltag36_11_12)
+
+# apriltag36_11_13 = [[0,0,0,0,0,0,0,0,0,0],\
+#                     [0,1,1,1,1,1,1,1,1,0],\
+#                     [0,1,0,1,1,1,0,1,1,0],\
+#                     [0,1,1,1,0,1,0,0,1,0],\
+#                     [0,1,0,1,0,0,1,0,1,0],\
+#                     [0,1,0,0,1,1,1,1,1,0],\
+#                     [0,1,0,1,1,1,1,0,1,0],\
+#                     [0,1,0,0,0,0,0,1,1,0],\
+#                     [0,1,1,1,1,1,1,1,1,0],\
+#                     [0,0,0,0,0,0,0,0,0,0]]
+
+# runTest('tag36_11_13', apriltag36_11_13)
+
+# apriltag36_11_14 = [[0,0,0,0,0,0,0,0,0,0],\
+#                     [0,1,1,1,1,1,1,1,1,0],\
+#                     [0,1,0,0,1,1,0,1,1,0],\
+#                     [0,1,0,1,1,0,1,0,1,0],\
+#                     [0,1,1,1,0,1,0,0,1,0],\
+#                     [0,1,1,1,0,1,1,0,1,0],\
+#                     [0,1,0,0,0,0,1,1,1,0],\
+#                     [0,1,0,1,1,0,1,0,1,0],\
+#                     [0,1,1,1,1,1,1,1,1,0],\
+#                     [0,0,0,0,0,0,0,0,0,0]]
+
+# runTest('tag36_11_14', apriltag36_11_14)
+
+# apriltag36_11_80 = [[0,0,0,0,0,0,0,0,0,0],\
+#                     [0,1,1,1,1,1,1,1,1,0],\
+#                     [0,1,1,0,0,1,0,1,1,0],\
+#                     [0,1,0,0,0,0,1,0,1,0],\
+#                     [0,1,1,1,1,1,0,1,1,0],\
+#                     [0,1,1,0,0,1,1,1,1,0],\
+#                     [0,1,0,1,1,1,1,1,1,0],\
+#                     [0,1,1,0,1,0,1,1,1,0],\
+#                     [0,1,1,1,1,1,1,1,1,0],\
+#                     [0,0,0,0,0,0,0,0,0,0]]
+
+# runTest('tag36_11_80', apriltag36_11_80)
+
+
+# apriltag36_11_544= [[0,0,0,0,0,0,0,0,0,0],\
+#                     [0,1,1,1,1,1,1,1,1,0],\
+#                     [0,1,0,0,0,0,1,1,1,0],\
+#                     [0,1,1,0,1,1,1,0,1,0],\
+#                     [0,1,0,0,1,0,0,1,1,0],\
+#                     [0,1,0,1,1,0,0,1,1,0],\
+#                     [0,1,1,0,0,0,1,1,1,0],\
+#                     [0,1,1,1,0,0,1,1,1,0],\
+#                     [0,1,1,1,1,1,1,1,1,0],\
+#                     [0,0,0,0,0,0,0,0,0,0]]
+
+# runTest('tag36_11_544', apriltag36_11_544)
+
+
+# test4 = []
+# print 'Enter 6 rows, each row MUST contain 6 entries of 1s and 0s ONLY!'
+# print 'EXAMPLE: 1 0 1 0 1 0'
+# '''
+# for i in range(6):
+#     row = input('Row #%s: ' % (i))
+#     row = row.split()
+#     test4.append(row)
+# runTest('#4', test4)
+# '''
